6ac77367d9ae2d976f39cc96388f0ea03c07cd58
Interstellar2 <33727643+Interstellar2@users.noreply.github.com>
2023-09-27 23:06:16 +0800

    Update qianfan.py

--- qianfan.py
+++ qianfan.py
@@ -54,3 +54,35 @@ class QianFan(LLM):
     ) -> str:
         text = self.get_result(prompt)
         return text
+
+
+class QianFanEmbeddings:
+    embed: Any
+    API_KEY = yaml_config["QianFanModel"]["API_KEY"]
+    SECRET_KEY = yaml_config["QianFanModel"]["SECRET_KEY"]
+
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+
+    def get_access_token(self):
+        url = f"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={self.API_KEY}&client_secret={self.SECRET_KEY}"
+
+        payload = json.dumps("")
+        headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.json().get("access_token")
+
+    def get_embed(self, texts: List[str]) -> List[List[float]]:
+        url = (
+            "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/embeddings/embedding-v1?access_token="
+            + self.get_access_token()
+        )
+
+        payload = json.dumps({"input": texts})
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        res = response.json()
+        return res["data"][0]["embedding"]
